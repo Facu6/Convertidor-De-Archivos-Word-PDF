e4d8d258704b0067a9755f61fc7049ed7d10f82a
Facu <dispenzafacu6@gmail.com>
2024-10-10 23:47:02 -0300

    Se agregan comentarios a script

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ from docx2pdf import convert
 from pdf2docx import Converter
 
 def generador_nombre_unico(carpeta, nombre_base):
+    """
+    Genera un nombre único para un archivo PDF para evitar sobreescritura.
     
+    Parámetros:
+    carpeta (str): Ruta de la carpeta donde se guardará el archivo.
+    nombre_base (str): Nombre base del archivo sin extensión.
+    
+    Retorna:
+    str: Ruta completa del archivo con un nombre único.
+    """
     contador = 0 
     nombre_archivo = f'{nombre_base}.pdf'
     ruta_archivo = os.path.join(carpeta, nombre_archivo)
@@ -16,13 +25,19 @@ def generador_nombre_unico(carpeta, nombre_base):
         ruta_archivo = os.path.join(carpeta, nombre_archivo)
     return ruta_archivo
 
-
-def convertidor_archivos(carpeta_origen, carpeta_destino, archivos_docx_individuales = None):
-
+def convertidor_archivos(carpeta_origen, carpeta_destino, archivos_docx_individuales=None):
+    """
+    Convierte archivos .docx a PDF y los guarda en la carpeta destino.
+    
+    Parámetros:
+    carpeta_origen (str): Carpeta que contiene archivos .docx.
+    carpeta_destino (str): Carpeta donde se guardarán los archivos PDF.
+    archivos_docx_individuales (list): Lista de archivos .docx individuales a convertir.
+    """
     # Inicializar COM
     pythoncom.CoInitialize()
 
-    # Condicional para manejar archivos "docx" indivuales    
+    # Si se proporcionan archivos .docx individuales
     if archivos_docx_individuales:
         temp_folder = "temp"
         if not os.path.exists(temp_folder):
@@ -35,40 +50,43 @@ def convertidor_archivos(carpeta_origen, carpeta_destino, archivos_docx_individu
             
             # Convertir el archivo temporal a PDF y guardarlo en la carpeta de destino
             convert(archivo_temp, carpeta_destino)
-            st.write(f'* El archivo --{archivo_temp}-- ha sido convertido con éxito a PDF. \nEl mismo ha sido guardado en la ruta --{carpeta_destino}--.')
             
-            # Eliminar el archivo temporal despues de la conversión
+            # Eliminar el archivo temporal después de la conversión
             os.remove(archivo_temp)
             
-        st.success('Conversión realizada con éxito!')
+        st.success('¡Conversión realizada con éxito!')
         
-    # Condicional para manejar carpetas con archivos "docx"     
+    # Si se proporciona una carpeta de origen con archivos .docx
     elif os.path.isdir(carpeta_origen):
         archivos_docx = [archivo for archivo in os.listdir(carpeta_origen) if archivo.endswith('.docx')]
         for archivo in archivos_docx:
-            convert(archivo, carpeta_destino)
-            st.write(f'* El archivo --{archivo}-- ha sido convertido con exito a PDF. \nEl mismo ha sido guardado en la ruta --{carpeta_destino}--.')
+            convert(os.path.join(carpeta_origen, archivo), carpeta_destino)
+            st.write(f'* El archivo --{archivo}-- ha sido convertido con éxito a PDF. \nEl mismo ha sido guardado en la ruta --{carpeta_destino}--.')
     
-        st.success('Conversión realizada con éxito!')
+        st.success('¡Conversión realizada con éxito!')
     
     else:
         st.write('ERROR: No se encontró la carpeta y/o el archivo.')
     
-    # Desinizializar COM
+    # Desinicializar COM
     pythoncom.CoUninitialize()
-    
 
-def unir_pdfs(lista_archivos_pdf, carpeta_destino_pdf, nombre_base = 'pdfs_unidos'):
+def unir_pdfs(lista_archivos_pdf, carpeta_destino_pdf, nombre_base='pdfs_unidos'):
+    """
+    Une múltiples archivos PDF en uno solo y lo guarda en la carpeta destino.
     
+    Parámetros:
+    lista_archivos_pdf (list): Lista de archivos PDF a unir.
+    carpeta_destino_pdf (str): Carpeta donde se guardará el archivo PDF unido.
+    nombre_base (str): Nombre base del archivo PDF unido.
+    """
     # Crear un objeto PdfWriter
     pdf_writer = PyPDF2.PdfWriter()
     
     # Iterar a través de los archivos PDF
     if lista_archivos_pdf:
-                
         for archivo in lista_archivos_pdf:
             pdf_reader = PyPDF2.PdfReader(archivo)
-            
             for pagina in range(len(pdf_reader.pages)):
                 pdf_writer.add_page(pdf_reader.pages[pagina])
         
@@ -77,19 +95,26 @@ def unir_pdfs(lista_archivos_pdf, carpeta_destino_pdf, nombre_base = 'pdfs_unido
         with open(archivo_salida, 'wb') as salida:
             pdf_writer.write(salida)
         
-        st.success('Unión realizada con éxito!')
-        
+        st.success('¡Unión realizada con éxito!')
     else:
-        st.write(f"La ruta {lista_archivos_pdf} no es un válida.")
-
+        st.write(f"La ruta {lista_archivos_pdf} no es válida.")
 
 def convertir_pdf_word(pdf, nombre_salida):
+    """
+    Convierte un archivo PDF a Word y lo guarda con el nombre especificado.
     
-    cv = Converter(pdf)
-    cv.convert(nombre_salida, start = 0, end = None)
+    Parámetros:
+    pdf (UploadedFile): Archivo PDF subido.
+    nombre_salida (str): Nombre del archivo de salida .docx.
+    """
+    # Guarda el archivo PDF subido en una ubicación temporal
+    with open(pdf.name, "wb") as f:
+        f.write(pdf.getbuffer())
+    
+    # Crea un convertidor para el archivo PDF subido
+    cv = Converter(pdf.name)
+    cv.convert(nombre_salida, start=0, end=None)
     cv.close()
-    
-    
 
 def main():
     st.sidebar.title('Navegación')
@@ -98,28 +123,25 @@ def main():
     if app_mode == 'Convertidor Word a PDF':
         st.title('Convertidor Word a PDF')
         # Input del usuario para seleccionar archivos ".docx"
-        archivos_docx_individuales = st.file_uploader('Seleccione archivos word', type = 'docx', accept_multiple_files= True)
+        archivos_docx_individuales = st.file_uploader('Seleccione archivos Word', type='docx', accept_multiple_files=True)
 
         # Inputs del usuario para seleccionar carpeta_origen y carpeta_destino
-        carpeta_origen = st.text_input('Ingrese la ruta de la carpeta donde se encuentran los archivos ".docx"') 
+        carpeta_origen = st.text_input('En caso de desear convertir varios archivos de una sola carpeta, ingrese dicha carpeta.') 
         carpeta_destino = st.text_input('Ingrese la carpeta destino donde se guardarán los archivos PDF.')     
             
-            
-        # Boton para iniciar la conversación 
+        # Botón para iniciar la conversión
         if st.button('Convertir'):
             if not archivos_docx_individuales and not carpeta_origen:
-                st.error('Por favor, seleccione un archivo Word o una carpeta la cual posea archivos Word.')
+                st.error('Por favor, seleccione un archivo Word o una carpeta que contenga archivos Word.')
             elif not carpeta_destino:
-                st.error('Por favor, ingrese la ruta con la carpeta de destino donde se descargarán los archivos PDF.')
+                st.error('Por favor, ingrese la ruta con la carpeta de destino donde se guardarán los archivos PDF.')
             else:
                 convertidor_archivos(carpeta_origen, carpeta_destino, archivos_docx_individuales)
     
     if app_mode == 'Convertidor PDF a Word':
-        
         st.title('Convertidor PDF a Word')
         
-        lista_archivos_word = st.file_uploader('Seleccione los archivos PDF para convertir:', type = 'pdf', accept_multiple_files= True)
-
+        lista_archivos_word = st.file_uploader('Seleccione los archivos PDF para convertir:', type='pdf', accept_multiple_files=True)
             
         if st.button('Convertir'):
             if not lista_archivos_word:
@@ -127,38 +149,26 @@ def main():
             else:
                 for archivo in lista_archivos_word:
                     nombre_salida = f'{archivo.name[:-4]}.docx'
-                    convertir_pdf_word(archivo, nombre_salida)  # Aquí 'archivo' es el objeto del archivo subido
-
+                    convertir_pdf_word(archivo, nombre_salida)
                     st.success(f'El archivo {archivo.name} fue convertido con éxito.')
         
     if app_mode == 'Unir PDFs':
         st.title('Unir PDFs')
-        # Input del usuario para seleccionar archivos ".docx"
-        lista_archivos_pdf = st.file_uploader('Seleccione archivos PDF', type = 'pdf', accept_multiple_files= True)
-               
-        # Input del usuario para seleccionar la carpeta de destino
-        carpeta_destino_pdf = st.text_input('Ingrese la carpeta destino donde se guardarán los archivos PDF.')     
+        lista_archivos_pdf = st.file_uploader('Seleccione archivos PDF', type='pdf', accept_multiple_files=True)
+        carpeta_destino_pdf = st.text_input('Ingrese la carpeta destino donde se guardarán los archivos PDF.')
         
         if lista_archivos_pdf:
-            # Mostrar los archivos cargados en una selección
             archivos_nombres = [archivo.name for archivo in lista_archivos_pdf]
             archivos_seleccionados = st.multiselect('Ordenar archivos:', options=archivos_nombres)
-
-            # Reordenar archivos según la selección
-            archivos_ordenados = [archivo for nombre in archivos_seleccionados 
-                                for archivo in lista_archivos_pdf 
-                                if archivo.name == nombre]
+            archivos_ordenados = [archivo for nombre in archivos_seleccionados for archivo in lista_archivos_pdf if archivo.name == nombre]
         
-            # Boton para iniciar la conversación 
             if st.button('Unir'):
                 if not archivos_ordenados:
-                    st.error('Por favor, seleccione PDF para ser unido.')
+                    st.error('Por favor, seleccione PDF para unir.')
                 elif not carpeta_destino_pdf:
-                    st.error('Por favor, ingrese la ruta con la carpeta de destino donde se descargarán los archivos PDF.')
+                    st.error('Por favor, ingrese la ruta con la carpeta de destino donde se guardarán los archivos PDF.')
                 else:
-                    unir_pdfs(lista_archivos_pdf, carpeta_destino_pdf)        
+                    unir_pdfs(archivos_ordenados, carpeta_destino_pdf)
 
-
-        
 if __name__ == "__main__":
     main()
